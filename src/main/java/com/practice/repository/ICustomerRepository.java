d2c6e65a6332f0cc2c228fe2e397c2cc4be31309
NG291 <you@example.com>
2024-08-26 13:44:48 +0700

    restful

---
+++ src/main/java/com/practice/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package com.practice.repository;
+
+import com.practice.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
---
+++ src/main/java/com/practice/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.practice.service;
+
+import com.practice.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
